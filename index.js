bf7166b8179c017f81a1313cc731da87f4c0e253
thembekageorge <thembiegeorge@gmail.com>
2017-08-14 16:16:31 +0200

    added route for counter and removed duplicity

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ var express = require('express');
 var app = express();
 var list = [];
 var noDuplicity = [];
+var counter = 0;
+var name ;
 
 // create a route
 app.get('/', function (req, res) {
- res.send('Hello World!');
+ res.send('You did not enter a name');
 });
 
 app.get('/greetings/:user', function(req, res){
  res.send('Hello ' + req.params.user);
- var name = req.params.user;
- list.push(name);
+  name = req.params.user;
+if(noDuplicity.indexOf(name) === -1){
+  noDuplicity.push(name)
+}
+   list.push(name);
+   counter++;
 });
 
-app.get('/greeted', function(req, res) {
-res.send(list);
+app.get('/greeted/', function(req, res) {
+res.send(noDuplicity);
 });
 
-//start the server
+app.get('/counter/:user', function(req, res) {
+res.send('You have been greeted: ' + counter + ' times.');
+});
+
+
 var server = app.listen(3000, function () {
 
  var host = server.address().address;
  var port = server.address().port;
 
- console.log('Example app listening at http://%s:%s', host, port);
+ console.log('Listening at http://%s:%s', host, port);
 
 });
